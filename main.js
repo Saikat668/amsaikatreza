03be83cd8fbb92851a50730f2373f4cee0739eee
A. M. SAIKAT REZA <144786257+Saikat668@users.noreply.github.com>
2026-09-26 16:08:44 +0600

    Add new photo memory to memory grid

--- main.js
+++ main.js
@@ -348,6 +348,7 @@ const memoryGrid = document.getElementById('memoryGrid');
 if (memoryGrid) {
   const MEMORIES = [
     { type: 'photo', src: 'memory/Last_Class_2026-08-31_at_23.21.24.jpeg', caption: 'Campus, 2025' },  // { type: 'photo', src: 'memory/example1.jpg', caption: 'Campus, 2026' },
+    { type: 'photo', src: 'memory/IMG20250618164304', caption: 'Campus, 2025' },
     // { type: 'youtube', id: 'dQw4w9WgXcQ', caption: 'Department program' },
     { type: 'video', src: 'memory/TOM_ai.mp4', caption: 'My TOM' }// { type: 'video', src: 'memory/clip1.mp4', caption: 'Farewell' },
   ];
